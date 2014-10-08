b8ba972e34cc6dbf66586e06700f0f41c5364eca
Jason McBride <jsononrails@gmail.com>
2014-10-07 23:37:51 -0400

    initial commit

---
+++ networking/net-watcher-unix.js
@@ -0,0 +1,36 @@
+'use strict';
+const
+  fs = require('fs'),
+  net = require('net'),
+  
+  filename = process.argv[2],
+  
+  server = net.createServer(function(connection) {
+    // reporting
+	console.log('Subscriber connected.');
+	connection.write("Now watching '" + filename + "' for changes...\n");
+	
+	// watcher setup
+	let watcher = fs.watch(filename, function() {
+		connection.write("File '" + filename + "' changed: " + Date.now() + "\n");
+	});
+	
+	// cleanup
+	connection.on('close', function() {
+		console.log('Subscriber disconnected');
+	});
+  });
+
+if (!filename) {
+	throw Error('No target filename was specified');
+}
+
+server.listen('/tmp/watcher.sock', function() {
+	console.log('Listening for subscribers...');
+});
+
+/*
+server.listen(5439, function() {
+	console.log('Listening for subscribers...');
+});
+*/
